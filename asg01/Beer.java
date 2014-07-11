251e35bd45f41a70f5b6e22656e260599f2c9288
quanghm93 <mquang.1611@gmail.com>
2014-07-11 09:01:47 +0700

    upload

---
+++ asg01/Beer.java
@@ -0,0 +1,12 @@
+package asg01;
+
+public class Beer {
+	public static void main(String [] args){
+		System.out.println("HoangMinhQuang\t11020304\tK56CC\tquanghm93\tmquang.1611@gmail.com");
+		for(int i=9;i>=1;i--){
+			System.out.println(i+" bottles of beer on the wall, "+i+"bottles of beer.\n	Take one down, pass it around,\n");
+		}
+		System.out.println("No more bottles of beer on the wall.");
+	}
+
+}
